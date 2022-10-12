636624129fdde5f9b31fd5486517150e66ce4478
Caio Vinícius <caio1525pereira@gmail.com>
2022-10-12 14:10:58 -0300

    feat: :sparkles: Componente Checkbox
    Componente Checkbox e documentação do componente no storybook

---
+++ src/components/Checkbox.stories.tsx
@@ -0,0 +1,12 @@
+import type { Meta, StoryObj } from "@storybook/react";
+
+import { Checkbox } from "./Checkbox";
+
+export default {
+  title: "Components/Checkbox",
+  component: Checkbox,
+  args: {},
+  argTypes: {}
+} as Meta;
+
+export const Default: StoryObj = {};
