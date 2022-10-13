3d82d52255389b7409eafd14bd136c7b03580588
Caio Vinícius <caio1525pereira@gmail.com>
2022-10-13 20:15:21 -0300

    refactor: :truck: Mudança do conteúdo da página de login para o arquivo SignIn.tsx no diretório pages

---
+++ src/pages/SignIn.stories.tsx
@@ -0,0 +1,12 @@
+import type { Meta, StoryObj } from "@storybook/react";
+
+import { SignIn } from "./SignIn";
+
+export default {
+  title: "Pages/SignIn",
+  component: SignIn,
+  args: {},
+  argTypes: {}
+} as Meta;
+
+export const Default: StoryObj = {};
