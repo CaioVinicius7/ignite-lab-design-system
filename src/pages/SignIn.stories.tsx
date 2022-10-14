69207dc3bc28d79e3aaff6c602ab732b5230200c
Caio Vinícius <caio1525pereira@gmail.com>
2022-10-13 21:38:28 -0300

    test: :white_check_mark: Teste de preenchimento e envio do formulário da página SignIn

--- src/pages/SignIn.stories.tsx
+++ src/pages/SignIn.stories.tsx
@@ -1,4 +1,6 @@
+import { expect } from "@storybook/jest";
 import type { Meta, StoryObj } from "@storybook/react";
+import { within, userEvent, waitFor } from "@storybook/testing-library";
 
 import { SignIn } from "./SignIn";
 
@@ -9,4 +11,21 @@ export default {
   argTypes: {}
 } as Meta;
 
-export const Default: StoryObj = {};
+export const Default: StoryObj = {
+  play: async ({ canvasElement }) => {
+    const canvas = within(canvasElement);
+
+    userEvent.type(
+      canvas.getByPlaceholderText("Digite seu e-mail"),
+      "caio@gmail.com"
+    );
+
+    userEvent.type(canvas.getByPlaceholderText("**********"), "password123");
+
+    userEvent.click(canvas.getByRole("button"));
+
+    await waitFor(() => {
+      return expect(canvas.getByText("Login Realizado")).toBeInTheDocument();
+    });
+  }
+};
